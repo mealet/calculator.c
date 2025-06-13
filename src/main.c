84036315f654e3bbf5103729cd91fb72b7bbc223
mealet <jasonragonter@gmail.com>
2025-06-13 22:17:23 +0300

    added user cli

--- src/main.c
+++ src/main.c
@@ -5,32 +5,59 @@
 #include "parser/parser.h"
 
 #include "interpreter/interpreter.h"
-#include "utils/stringBuffer.h"
 
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT_LEN 255
+#define INPUT_ARROW "> "
 
 int main() {
-  char *input = "2 * (1 + 1)";
+  // initializing empty input buffer
+  char input[MAX_INPUT_LEN + 1];
 
-  __attribute__((cleanup(token_freeBuffer))) tokensBuffer tokens =
-      lexer_tokenize(input);
+  while (1) {
+    // printing this arrow (idk how to call it)
+    printf(INPUT_ARROW);
+    fflush(stdout);
 
-  size_t i;
-  for (i = 0; i < tokens.len; i++) {
-    token *tok = token_peekBuffer(&tokens, i);
-    printf("| `%s`: %s ", tok->value.ptr, token_display(tok));
+    // clearing buffer from previous iteration
+
+    memset(input, 0, sizeof(input));
+
+    // taking input
+    if (fgets(input, sizeof(input), stdin)) {
+      if (input[0] == '\0') {
+        continue;
+      }
+
+      // removing new line ascii symbol
+
+      input[strcspn(input, "\n")] = '\0';
+
+      // exit option
+      if (!strcmp(input, "exit")) {
+        break;
+      }
+
+      // analyzing with lexer
+      __attribute__((cleanup(token_freeBuffer))) tokensBuffer tokens =
+          lexer_tokenize(input);
+
+      // parsing with parser
+      parser parser = parser_new(tokens);
+      __attribute__((cleanup(cleanup_ast))) astNode expr =
+          parser_parse(&parser);
+
+      if (expr.type == None) {
+        return 1;
+      }
+
+      // and then evaluating the result
+      uint32_t result = interpreter_eval(&expr);
+      printf("%d\n", result);
+    }
   }
-  printf("|\n\n");
-
-  parser parser = parser_new(tokens);
-  __attribute__((cleanup(cleanup_ast))) astNode expr = parser_parse(&parser);
-
-  if (expr.type == None) {
-    return 1;
-  }
-
-  uint32_t result = interpreter_eval(&expr);
-  printf("RESULT: %d\n", result);
 
   return 0;
 }
